3a76c9d818c47b316de6c7b72537f4a56b205323
sultanfarooq1 <sultanfarooq71@outlook.com>
2020-07-19 17:30:40 +0500

    Update DBapi.py

--- DBapi.py
+++ DBapi.py
@@ -1,36 +1,40 @@
 import flask
+import psycopg2
 from flask import request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+
+#connecting to db
+con = psycopg2.connect(
+    host = "localhost",
+    database="Flaskapi",
+    user="postgres",
+    password="Beatlesdiary5632489")
+
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+
+
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql://postgres:Beatlesdiary56324@localhost/Flaskapi'
 db = SQLAlchemy(app)
 
-
 results = []
-
-class Information(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    text = db.Column(db.String(120),unique = False)
-    
-    def __init__(self,text):
-        self.text = text
-
 
 @app.route('/reverse', methods=['POST'])
 def home():
     user = request.form['ttrs']
     print (user[::-1])
     results.append(user)
+    #cursor
+    cur = con.cursor()
+    cur.execute("insert into information (info) values (%s)",(results))
+    con.commit()
+    cur.close()
     return user[::-1]
-    
-    info = Information(user)
-    
-    db.session.add(info)
-    db.session.commit()
     return jsonify(results)
+    
 
 if __name__ == '__main__':
     app.run()
